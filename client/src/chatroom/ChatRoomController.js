1ae3e87ddf5a178b2e91aa96e211725d78923cc0
Bragi Bergþórsson <bragur@gmail.com>
2016-02-21 14:01:05 +0000

    Implemented all actions in channel

--- client/src/chatroom/ChatRoomController.js
+++ client/src/chatroom/ChatRoomController.js
@@ -1,88 +1,112 @@
 'use strict';
 
-angular.module("Chatroom").controller("ChatRoomController", 
-function($scope, $rootScope, $stateParams, SocketService, SharedProperties) {
+angular.module("Chatroom").controller("ChatRoomController",
+    function($scope, $rootScope, $stateParams, SocketService, SharedProperties) {
 
-	console.log("Im in the ChatRoomController");
+        console.log("Im in the ChatRoomController");
 
-	//$scope.chatMsg = "";
+        //$scope.chatMsg = "";
 
-	$scope.chatroomName = $stateParams.ChatroomName;
+        $scope.chatroomName = $stateParams.ChatroomName;
 
-	var joinObj = {
-		room: $scope.chatroomName,
-		pass: ''
-	};
+        var joinObj = {
+            room: $scope.chatroomName,
+            pass: ''
+        };
 
-	SocketService.emit('joinroom', joinObj, function (allowed, reason) {
-		console.log("Attempting to join " + $scope.chatroomName);
-		if (allowed) {
-			// Updated info on recently joined room
+        SocketService.emit('joinroom', joinObj, function(allowed, reason) {
+            console.log("Attempting to join " + $scope.chatroomName);
+            if (allowed) {
+                // Updated info on recently joined room
 
-			// Updated messageHistory
-			/*SocketService.on('updatechat', function (room, messageHistory) {
-				$scope.msgHistory = messageHistory;
-				console.log(messageHistory);
-			});*/
+            } else {
+                console.log("Not allowed to enter because " + reason);
+            }
+        });
 
-			SocketService.on('updatetopic', function (room, topic, username) {
-				if (room === $scope.chatroomName) {
-					console.log(username + " updated topic in " + room + " to: " + topic);
-				}
-			});
+        setTimeout(function() {
+            $scope.chatroomTopic = SharedProperties.getTopic($scope.chatroomName);
+            console.log($scope.chatroomTopic);
+            $scope.$apply();
+        }, 200);
 
-			SocketService.on('servermessage', function (status, room, username) {
-				if (room === $scope.chatroomName) {
-					console.log(username + " " + status + " " + room);
-				}
-			});
+        $scope.sendMessage = function sendMessage(mess) {
 
-		} else {
-			console.log("Not allowed to enter because " + reason);
-		}
-	});
+            if (mess.charAt(0) !== '/') {
+                var data = {
+                    roomName: $scope.chatroomName,
+                    msg: mess
+                };
 
-	setTimeout(function() { 
-		$scope.chatroomTopic = SharedProperties.getTopic($scope.chatroomName);
-		console.log($scope.chatroomTopic);
-		$scope.$apply(); 
-	}, 200);
+                console.log("Sending msg to " + data.roomName + ": " + mess);
 
-	$scope.sendMessage = function sendMessage(mess) {
-		//$scope.msgHistory = SharedProperties.getRoomMsgHistory($scope.chatroomName);
+                SocketService.emit('sendmsg', data);
+            } else {
+                var action = mess.split(" ")[0].substring(1);
+                var line = mess.substring(action.length + 2);
+                var params = line.split(" ");
+                // console.log("Action: " + action + ", params: " + line);
+                // console.log(params);
 
-		var data = {
-			roomName: $scope.chatroomName,
-			msg: mess
-		};
+                var obj = {};
 
-		console.log("Sending msg to " + data.roomName + ": " + mess);
+                if (action === 'topic') {
+                    action = 'settopic';
+                    obj = {
+                        topic: line,
+                        room: $scope.chatroomName
+                    };
+                } else {
+                    obj = {
+                        user: params[0],
+                        room: $scope.chatroomName
+                    };
+                }
 
-		SocketService.emit('sendmsg', data);
-		$scope.chatMsg = "";
+                if (obj['room'].toString().length > 0) {
+                    SocketService.emit(action, obj, function(success) {
+                        console.log(action + " successful!");
+                    });
+                }
+            }
 
-	};
+            $scope.chatMsg = "";
+        };
 
-	SocketService.on('updatechat', function (roomName, messageHistory) {
-		if (roomName === $scope.chatroomName) {
-			console.log("messagehistory is: ");
-			console.log(messageHistory);
-			$scope.msgHistory = messageHistory;
-		}
-	});
+        SocketService.on('servermessage', function(status, room, username) {
+            if (room === $scope.chatroomName) {
+                console.log(username + " " + status + " " + room);
+            }
+        });
 
-	SocketService.on('updateusers', function (room, users, ops) {
-		var chatroom = SharedProperties.getRoom(room);
+        SocketService.on('updatechat', function(roomName, messageHistory) {
+            if (roomName === $scope.chatroomName) {
+                console.log("messagehistory is: ");
+                console.log(messageHistory);
+                $scope.msgHistory = messageHistory;
+            }
+        });
 
-		if (room === $scope.chatroomName) {
-			$scope.users = chatroom.users;
-			$scope.ops = chatroom.ops;
-			var updatedUsers = {
-				Users: $scope.users,
-				Ops: $scope.ops,
-				Room: room
-			};
-			console.log(updatedUsers);
-		}
-	});
-});
+        SocketService.on('updatetopic', function(room, topic, username) {
+            if (room === $scope.chatroomName) {
+                $scope.chatroomTopic = topic;
+                // $scope.$apply();
+                console.log(username + " set topic in " + room + " to " + topic);
+            }
+        });
+
+        SocketService.on('updateusers', function(room, users, ops) {
+            var chatroom = SharedProperties.getRoom(room);
+
+            if (room === $scope.chatroomName) {
+                $scope.users = chatroom.users;
+                $scope.ops = chatroom.ops;
+                var updatedUsers = {
+                    Users: $scope.users,
+                    Ops: $scope.ops,
+                    Room: room
+                };
+                console.log(updatedUsers);
+            }
+        });
+    });
