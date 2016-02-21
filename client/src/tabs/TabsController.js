b3ca8f515512c6e39adf714e2c828cea96beeda1
Bragi Bergþórsson <bragur@gmail.com>
2016-02-21 17:02:11 +0000

    Private messages get alert now

---
+++ client/src/tabs/TabsController.js
@@ -0,0 +1,21 @@
+angular.module('Chatroom').controller('TabsController', function($scope, SocketService) {
+	$scope.msgHeader = "Private Messages";
+	$scope.seen = 0;
+	$scope.unseen = false;
+
+	$scope.chosen = function chosen(tab) {
+		console.log("Switching to tab " + tab);
+		$scope.seen = tab;
+
+		if ($scope.seen === 1 && $scope.unseen === true) {
+			$scope.msgHeader = "Private Messages";
+		}
+	};
+
+	SocketService.on('recv_privatemsg', function(from, msg) {
+		if ($scope.seen === 0) {
+			$scope.unseen = true;
+			$scope.msgHeader = "New Msg!";
+		}
+	});
+});
